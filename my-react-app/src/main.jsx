6f5f0aa1a2ba293b63188e17599b014465140644
Yvann Prime <yvannprime3@gmail.com>
2026-01-09 15:13:01 +0100

    Mise en place des routes

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/Home/index.jsx'
+import Home from './pages/Home'
+import About from './pages/About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Error404 from './pages/Error404'
+import Header from './components/Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+    </Router>
   </StrictMode>,
 )
---
+++ my-react-app/src/pages/Error404/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>Error 404</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default Error404
