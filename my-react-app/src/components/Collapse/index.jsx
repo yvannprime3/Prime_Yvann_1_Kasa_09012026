95f7d38726e7e7a42dd5ada12ec6785d937269b2
Yvann Prime <yvannprime3@gmail.com>
2026-01-28 14:28:40 +0100

    Components Collapse

---
+++ my-react-app/src/components/Collapse/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from "react"
+import styles from "./Collapse.module.scss"
+
+const Collapse = (title, content) => {
+    const [open, setOpen] = useState(false)
+  return (
+    <div className={styles.collapse}>
+      <button className={styles.collapse__header} onClick={() => setOpen(!open)}>
+        <span>{title}</span>
+        <span className={`arrow ${open ? "open" : ""}`}>⌃</span>
+      </button>
+
+      {open && (
+        <div className={styles.collapse__content}>
+          {Array.isArray(content)
+            ? content.map((item, index) => <p key={index}>{item}</p>)
+            : <p>{content}</p>}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Collapse
