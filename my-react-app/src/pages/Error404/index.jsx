df1b07876b771ad348a3b381ef1e2a09ed0fed73
Yvann Prime <yvannprime3@gmail.com>
2026-01-28 11:47:44 +0100

    Page d'erreur

--- my-react-app/src/pages/Error404/index.jsx
+++ my-react-app/src/pages/Error404/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from "./Error404.module.scss"
 
 const Error404 = () => {
   return (
-    <div>
-      <h1>Error 404</h1>
-      <p>Oups! La page que vous demandez n'existe pas</p>
+    <main className={styles.error}>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/">Retourner sur la page d'acceuil</Link>
-    </div>
+    </main>
   )
 }
 
