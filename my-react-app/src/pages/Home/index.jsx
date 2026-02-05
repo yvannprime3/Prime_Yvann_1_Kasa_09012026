a37089f7f6401436cbdce8675150688e4649e3bf
Yvann Prime <yvannprime3@gmail.com>
2026-02-05 09:05:17 +0100

    Responsive

--- my-react-app/src/pages/Home/index.jsx
+++ my-react-app/src/pages/Home/index.jsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom"
 import Banner from "../../components/Banner"
 import Gallery from "../../components/Gallery"
 import banner1 from "/src/assets/banner1.png"
+import styles from "./Home.module.scss"
 
 function Home() {
 
   return (
     <>
-      <Banner imageURL={banner1} banner="Chez nous, partout et ailleurs" />
-      <Gallery />
+      <div className={styles.home}>
+        <Banner imageURL={banner1} banner="Chez nous, partout et ailleurs" />
+        <Gallery />
+      </div>
     </>
   )
 }
